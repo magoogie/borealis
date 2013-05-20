42e74843a4af26a75625ef4f71136ba9672c0de1
Magoo <indigo.gecko@gmail.com>
2013-05-20 23:12:15 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Borealis::Application.config.secret_token = '441e65642b6243f7c027f918c330f544846eed7e8bafb53c519986f6ef42b26409bd157f64c0f214f9e470b28dff5032101c7315314de716ea173a523e3e5d5e'
